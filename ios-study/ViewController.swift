b98bd435c5ff037bcda1d39b66a769d6717082fa
yongtae <>
2021-12-21 14:01:59 +0900

    프로젝트 생성

---
+++ ios-study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-study
+//
+//  Created by 김용태 on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
